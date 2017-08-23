69dd5c42d254daf302fff44403ac47ad7c4b0ac1
xuwei-k <6b656e6a69@gmail.com>
2017-08-23 23:35:52 +0900

    scalafmt

--- src/main/scala/sbt_conflict_classes/ConflictClassesPlugin.scala
+++ src/main/scala/sbt_conflict_classes/ConflictClassesPlugin.scala
@@ -2,8 +2,8 @@ package sbt_conflict_classes
 
 import java.io.File
 
-case class Classpath(asFile:File) {
-  def listResources():Seq[Resource] = {
+case class Classpath(asFile: File) {
+  def listResources(): Seq[Resource] = {
     asFile match {
       case f if f.isDirectory => listFromDirectory(f)
       case f if f.isFile => listFromJar(f)
@@ -11,33 +11,34 @@ case class Classpath(asFile:File) {
     }
   }
 
-  private[this] def listFromDirectory(root:File, prefix:Seq[String] = Seq()):Seq[Resource] = {
+  private[this] def listFromDirectory(root: File, prefix: Seq[String] = Seq()): Seq[Resource] = {
     import scala.collection.JavaConverters._
     root.listFiles().flatMap { f =>
       f match {
-        case f if(f.isFile) => Seq(Resource((prefix :+ f.getName).mkString("/")))
-        case f if(f.isDirectory) => listFromDirectory(f, prefix :+ f.getName)
+        case f if (f.isFile) => Seq(Resource((prefix :+ f.getName).mkString("/")))
+        case f if (f.isDirectory) => listFromDirectory(f, prefix :+ f.getName)
       }
     }
   }
 
-  private[this] def listFromJar(file:File) = {
+  private[this] def listFromJar(file: File) = {
     import scala.collection.JavaConverters._
-    new java.util.jar.JarFile(file)
-      .entries.asScala
-      .map { e => Resource(e.getName) }
-      .toSeq
+    new java.util.jar.JarFile(file).entries.asScala.map { e =>
+      Resource(e.getName)
+    }.toSeq
   }
 }
-case class Resource(name:String)
-case class Conflict(resources:Set[Resource], classpathes:Set[Classpath])
+case class Resource(name: String)
+case class Conflict(resources: Set[Resource], classpathes: Set[Classpath])
 
 object ConflictClassesPlugin extends sbt.AutoPlugin {
   import sbt._
 
   object autoImport {
     val conflictClasses = TaskKey[Seq[Conflict]]("conflict-classes", "Show conflict classes in classpath")
-    val conflictClassExcludes = TaskKey[Seq[String]]("conflict-class-excludes", "Exclude pattern for conflict checking. Check is done by `path startWith pattern`.")
+    val conflictClassExcludes = TaskKey[Seq[String]](
+      "conflict-class-excludes",
+      "Exclude pattern for conflict checking. Check is done by `path startWith pattern`.")
   }
 
   import autoImport._
@@ -47,17 +48,21 @@ object ConflictClassesPlugin extends sbt.AutoPlugin {
       conflictClassExcludes := Seq("META-INF/")
     )
 
-  def forConfig(config:Configuration) = inConfig(config)(Seq(
-    conflictClasses := {
-      val conflicts = buildConflicts((Keys.dependencyClasspath in config).value.map(cp => Classpath(cp.data)), conflictClassExcludes.value)
-      printConflicts(Keys.streams.value.log, conflicts)
-      conflicts
-    }
-  ))
+  def forConfig(config: Configuration) =
+    inConfig(config)(
+      Seq(
+        conflictClasses := {
+          val conflicts = buildConflicts(
+            (Keys.dependencyClasspath in config).value.map(cp => Classpath(cp.data)),
+            conflictClassExcludes.value)
+          printConflicts(Keys.streams.value.log, conflicts)
+          conflicts
+        }
+      ))
 
-  def printConflicts(log:Logger, conflicts:Seq[Conflict]):Unit = {
+  def printConflicts(log: Logger, conflicts: Seq[Conflict]): Unit = {
     log.info("Listing conflict classes:")
-    conflicts.foreach {conflict:Conflict =>
+    conflicts.foreach { conflict: Conflict =>
       log.info("Found conflict classes in:")
       conflict.classpathes.toSeq.sortBy(_.asFile.name).foreach { jar =>
         log.info("    " + jar.asFile.getPath)
@@ -69,25 +74,25 @@ object ConflictClassesPlugin extends sbt.AutoPlugin {
     }
   }
 
-  def buildConflicts(cps:Seq[Classpath], excludes:Seq[String]):Seq[Conflict] = {
-    val resourceToCps:Map[Resource, Seq[Classpath]] =
-      cps.foldLeft(Map[Resource, Seq[Classpath]]()) {(map, cp) =>
-        cp
-          .listResources
-          .filter { res =>
-            !res.name.endsWith("/") && !excludes.exists(ex => res.name.startsWith(ex))
-          }
-          .foldLeft(map) {(map, res) => map + (res -> (map.getOrElse(res, Seq()) :+ cp)) }
+  def buildConflicts(cps: Seq[Classpath], excludes: Seq[String]): Seq[Conflict] = {
+    val resourceToCps: Map[Resource, Seq[Classpath]] =
+      cps.foldLeft(Map[Resource, Seq[Classpath]]()) { (map, cp) =>
+        cp.listResources.filter { res =>
+          !res.name.endsWith("/") && !excludes.exists(ex => res.name.startsWith(ex))
+        }.foldLeft(map) { (map, res) =>
+          map + (res -> (map.getOrElse(res, Seq()) :+ cp))
+        }
       }
 
-    val cpsToResources:Map[Set[Classpath], Set[Resource]] =
+    val cpsToResources: Map[Set[Classpath], Set[Resource]] =
       resourceToCps.foldLeft(Map[Set[Classpath], Set[Resource]]()) { (map, kv) =>
-      kv match { case (res, cps) =>
-        val cpSet = cps.toSet
-        map + (cpSet -> (map.getOrElse(cpSet, Set()) + res))
+        kv match {
+          case (res, cps) =>
+            val cpSet = cps.toSet
+            map + (cpSet -> (map.getOrElse(cpSet, Set()) + res))
+        }
       }
-    }
 
-    cpsToResources.filter { case (cps, _) => cps.size > 1 }.map {case (cps, resources) => Conflict(resources, cps) }.toSeq
+    cpsToResources.filter { case (cps, _) => cps.size > 1 }.map { case (cps, resources) => Conflict(resources, cps) }.toSeq
   }
 }
